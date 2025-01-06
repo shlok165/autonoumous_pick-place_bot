01f21e2d51c52ea102e71be7c097a1f88d000126
harshitkalra03 <harshit.kalra03@gmail.com>
2025-01-06 22:27:11 +0530

    updated urdf

--- build/robot_controller/prefix_override/sitecustomize.py
+++ build/robot_controller/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/slok/autonoumous_pick-place_bot/install/robot_controller'
+    sys.prefix = sys.exec_prefix = '/home/harshitkalra/autonoumous_pick-place_bot/install/robot_controller'
